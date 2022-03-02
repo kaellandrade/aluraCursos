f1f3e87420af33e74322290cb0cf0eb354724729
Raphael Cabral <brunoraphael@moobitech.com.br>
2022-03-02 14:37:47 -0300

    merge 3 and 4

--- algoritmos/src/TestaMerge.js
+++ algoritmos/src/TestaMerge.js
@@ -1,6 +1,10 @@
 const Nota = require("./Nota");
 const merge1 = require("./merge1");
 const merge2 = require("./merge2");
+const merge3 = require("./merge3OrderNames");
+const TestaOrdenacaoAoIntercalar = require("./TestaOrdenacaoAoIntercalar");
+
+
 
 function imprimeName(rank) {
   rank.forEach((element) => {
@@ -26,10 +30,30 @@ const notasDoAlberto = [
   new Nota("bob", 1000),
 ];
 
-// ------Mege 1------
-const arrayUnicoNotas = [...notasDoMauricio, ...notasDoAlberto];
+const names = [
+  new Nota("Andressa", null),
+  new Nota("Camila", null),
+  new Nota("Enzo", null),
+  new Nota("Fernando", null),
+  new Nota("Maria", null),
+  new Nota("Alberto", null),
+  new Nota("Jonas", null),
+  new Nota("Junior", null),
+  new Nota("Paloma", null),
+  new Nota("Paulo", null)
+]
 
+// ----Merge 1----
 const rank1 = merge1(notasDoMauricio, notasDoAlberto);
-const rank2 = merge2(arrayUnicoNotas, 0, 4, arrayUnicoNotas.length);
 
+// ------Merge 2------
+const arrayUnicoNotas = [...notasDoMauricio, ...notasDoAlberto];
+/*
+const rank2 = merge2(arrayUnicoNotas, 0, 4, arrayUnicoNotas.length);
 imprimeName(rank2);
+*/
+// -----Merger 3 order by names-----
+const rank3 = merge3(names, 0, 5, names.length);
+TestaOrdenacaoAoIntercalar(names, 0, 5, names.length);
+
+// imprimeName(rank3);
